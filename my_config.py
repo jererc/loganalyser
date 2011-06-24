0b974584473785958bf55869bc4824dccafffdae
jererc <jererc@gmail.com>
2011-06-24 12:56:06 +0200

    third commit

--- my_config.py
+++ my_config.py
@@ -1,7 +1,7 @@
 import re
 
 path_log = '/home/je/Desktop/log/'
-re_log_line = re.compile(r'(?P<tF>.*?)\s(?P<tT>.*?)\|(?P<h>.*?)\|(?P<Uq>.*?)\|(?P<s>.*?)\|(?P<O>.*?)\|(?P<D>.*?)\|(?P<EDGE_DURATION>.*?)\|(?P<EDGE_START>.*?)\|(?P<EDGE_SENT>.*?)\|(?P<XGEOIP_COUNTRY_CODE2>.*?)\|(?P<Referer>.*?)\|(?P<User_agent>.*)', re.I)
+re_log_line = re.compile(r'(?P<t>.*?)\|(?P<h>.*?)\|(?P<Uq>.*?)\|(?P<s>.*?)\|(?P<O>.*?)\|(?P<D>.*?)\|(?P<EDGE_DURATION>.*?)\|(?P<EDGE_START>.*?)\|(?P<EDGE_SENT>.*?)\|(?P<XGEOIP_COUNTRY_CODE2>.*?)\|(?P<Referer>.*?)\|(?P<User_agent>.*)', re.I)
 re_request_path = re.compile(r'/(?P<user_id>.*?)/(?P<media_id>.*?)/(?P<asset_name>.*?)(\-(?P<timestamp>.*?))?\.(?P<extension>.*?)\?(?P<query_string>.*)', re.I)
 mongodb_server = '127.0.0.1'
 mongodb_collection = 'logs'
