3ff5c1dff78a2ddfa20dc4fa5ce68c10adb08358
jererc <jererc@gmail.com>
2011-06-26 13:08:10 +0200

    dynamic select values in the template

--- stats/views.py
+++ stats/views.py
@@ -4,8 +4,24 @@ import pymongo
 import my_config
 
 
+def get_stats_types(collection):
+    """Get all stats_types available in the mongodb collection"""
+    stats_types = {}
+    for d in collection.find({'stats_type': {'$exists': True}}):
+        if d['stats_type'] not in stats_types:
+            stats_types[d['stats_type']] = True
+    return sorted(stats_types.keys())
+
+
 def home(request):
     time_delta = my_config.STATS_TIME_DELTA
+    
+    #connect to the database
+    connection = pymongo.Connection(my_config.MONGODB_SERVER)
+    db = connection.test
+    collection = pymongo.collection.Collection(db, my_config.MONGODB_STATS_COLLECTION)
+    
+    stats_types = get_stats_types(collection)
     
     try:
         #get the post values from the form
@@ -14,16 +30,11 @@ def home(request):
     except:
         return render_to_response('base.html', locals(), context_instance=RequestContext(request))
     else:
-        #connect to the database
-        connection = pymongo.Connection(my_config.MONGODB_SERVER)
-        db = connection.test
-        collection = pymongo.collection.Collection(db, my_config.MONGODB_STATS_COLLECTION)
-
         #get the database document according to the stats type and time from the form
         document = collection.find_one({'stats_type': stats_type, 'stats_time': int(stats_time)})
         if document:
             stats = document['stats']
         else:
             stats = []
-        
+
         return render_to_response('content.html', locals(), context_instance=RequestContext(request))
